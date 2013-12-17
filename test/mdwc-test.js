db875a9f2f259623674666d6f85c91d9e2539edb
Fujisawa Shin <fujisawa-shin@kayac.com>
2013-12-18 02:08:39 +0900

    先祖さかのぼってのカウントにも対応。テストも追加。

--- test/mdwc-test.js
+++ test/mdwc-test.js
@@ -12,4 +12,18 @@ describe('mdwc', function () {
         assert.equal(result.sections[2].length, 8);
         assert.equal(result.sections[3].length, 16);
     });
+
+    it('count section complex chars', function () {
+        var result = countMarkdown('test/samples/sample2.md');
+        assert.equal(result.sections.length, 8);
+
+        assert.equal(result.sections[0].length, 24);
+        assert.equal(result.sections[1].length, 24);
+        assert.equal(result.sections[2].length, 12);
+        assert.equal(result.sections[3].length, 4);
+        assert.equal(result.sections[4].length, 4);
+        assert.equal(result.sections[5].length, 4);
+        assert.equal(result.sections[6].length, 6);
+        assert.equal(result.sections[7].length, 6);
+    });
 });
